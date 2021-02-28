e2d2d8da923da5388ef8ea3fadc191af3d318e65
Preminstrel <60024807+preminstrel@users.noreply.github.com>
2021-02-28 22:06:56 +0800

    new

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,6 +1,6 @@
 /** Performs some basic linked list tests. */
 public class LinkedListDequeTest {
-	/* Utility method for printing out empty checks. */
+/* Utility method for printing out empty checks. */
     public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
 			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
@@ -9,7 +9,7 @@ public class LinkedListDequeTest {
 		return true;
 	}
 
-	/* Utility method for printing out empty checks. */
+/* Utility method for printing out empty checks. */
 	public static boolean checkSize(int expected, int actual) {
 		if (expected != actual) {
 			System.out.println("size() returned " + actual + ", but expected: " + expected);
@@ -28,14 +28,14 @@ public class LinkedListDequeTest {
 		}
 	}
 
-	/** Adds a few things to the list, checking isEmpty() and size() are correct, 
-	  * finally printing the results. 
-	  *
-	  * && is the "and" operation. */
-	public static void addIsEmptySizeTest() {
+/** Adds a few things to the list, checking isEmpty() and size() are correct, 
+	 * finally printing the results. 
+	 *
+	 * && is the "and" operation. */
+     public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below " + 
-			"(and delete this print statement).");
+		System.out.println("Make sure to uncomment the lines below " 
+			+ "(and delete this print statement).");
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -65,8 +65,8 @@ public class LinkedListDequeTest {
 
 		System.out.println("Running add/remove test.");
 
-		System.out.println("Make sure to uncomment the lines below " + 
-			"(and delete this print statement).");
+		System.out.println("Make sure to uncomment the lines below " 
+			+ "(and delete this print statement).");
 
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty 
